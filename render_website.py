b2e0ff9278fd1867b7f894c9b3db68f0c1d06505
axmetES <fraktsia@gmail.com>
2020-09-14 17:11:57 +0600

    Some changes after review. render_website.py: - Deleted extra dependencies urllib.parse - Added dependencies: Path, glob. - Renamed variables: directory, file_name, site_page. - Added some variables: files_catalog, file_name, files_in_directory, books_on_page. - Added function: remove_old_files.
    template.html.
    - Added indent.
    - Changed variables all_pages.

--- render_website.py
+++ render_website.py
@@ -1,15 +1,17 @@
 import json
 import os
-from urllib.parse import quote, quote_from_bytes, urlencode, quote_plus
+import glob
+from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 from math import ceil
 
-directory = 'media'
-file_name = 'books_catalog.json'
-pages_directory = 'pages'
+json_directory = 'media'
+catalog_name = 'books_catalog.json'
+pages_directory = Path('pages')
+files_catalog = set()
 
 os.makedirs(pages_directory, exist_ok=True)
 
@@ -22,17 +24,26 @@ def get_catalog(directory, file_name):
         return books_catalog
 
 
+def remove_old_files(files_catalog, files_in_directory):
+    for file in files_in_directory:
+        if str(file) not in files_catalog:
+            os.remove(file)
+
+
 def on_reload():
+    books_on_page = 20
+    files_in_directory = pages_directory.glob('*.html')
+
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
 
-    books_catalog = get_catalog(directory, file_name)
-    pages = chunked(books_catalog, 20)
+    books_catalog = get_catalog(json_directory, catalog_name)
+    pages = chunked(books_catalog, books_on_page)
     all_pages = list(pages)
-    site_pages = len(all_pages)
+    pages_count = len(all_pages)
 
     for page_num, page in enumerate(all_pages, 1):
         half_page = len(page) / 2
@@ -41,13 +52,18 @@ def on_reload():
         rendered_page = template.render(
             first_books=first_catalog,
             second_books=second_catalog,
-            all_pages=site_pages,
+            pages_count=pages_count,
             current_page=page_num,
-
         )
-        index_directory = os.path.join(pages_directory, f'index{page_num}.html')
+
+        file_name = f'index{page_num}.html'
+        index_directory = os.path.join(pages_directory, file_name)
+        files_catalog.add(index_directory)
+
         with open(index_directory, 'w', encoding="utf8") as file:
             file.write(rendered_page)
+
+    remove_old_files(files_catalog, files_in_directory)
 
 
 on_reload()
